2e04b1591a0ea631e64be03d84c9c3a99dcc4883
gisele89 <giselebartolo@gmail.com>
2020-10-14 18:31:16 -0300

    agrega interfaz clothes.ts y cambios en template

--- src/app/clothes-list/clothes-list.component.ts
+++ src/app/clothes-list/clothes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Clothes } from './Clothes';
 
 @Component({
   selector: 'app-clothes-list',
@@ -7,14 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClothesListComponent implements OnInit {
 
-  clothes = {
-    name: 'Rock',
-    type: 'Campera de cuero',
-    price: 200,
-    stock: 5,
-    image: '/assets/campera-rock1.jpg'
-
-  };
+  clothingItems: Clothes [] = [
+    {
+      name: 'Rock',
+      type: 'Campera de cuero',
+      price: 200,
+      stock: 5,
+      image: '/assets/campera-rock1.jpg',
+      clearance: false,
+    },
+    {
+      name: 'RockStar',
+      type: 'Campera de cuero',
+      price: 400,
+      stock: 3,
+      image: '/assets/campera-rock1.jpg',
+      clearance: true,
+    },
+    {
+      name: 'PopRock',
+      type: 'Cartera de cuero',
+      price: 250,
+      stock: 0,
+      image: '/assets/campera-rock1.jpg',
+      clearance: false,
+    },
+  ] ;
   constructor() { }
 
   ngOnInit(): void {
